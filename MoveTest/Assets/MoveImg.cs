8ec3c6affe8b404a772d648da22b115eb97ed319
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-04-29 20:09:31 +0900

    Add MoveImage

---
+++ MoveTest/Assets/MoveImg.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveImg : MonoBehaviour {
+
+  float xSpeed = 0.2f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.Translate(this.xSpeed, 0, 0);
+	}
+}
