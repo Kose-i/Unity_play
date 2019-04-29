3ea050d464b7c6c6473826750e75889feaedbd7f
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-04-29 19:38:43 +0900

    Add RotateImg

---
+++ RotateTest/Assets/Image/GitprofileController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitprofileController : MonoBehaviour {
+
+  float rotSpeed = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    this.rotSpeed = 10;
+    transform.Rotate(0, 0, this.rotSpeed);
+	}
+}
