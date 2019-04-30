996a8f9aa0a9e99203b1b255c7865bac22073879
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-04-30 17:51:55 +0900

    Add dd Mouse Settup

---
+++ MouseTest/Assets/GitImageController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitImageController : MonoBehaviour {
+
+  float rotateSpeed = 0;
+  const int threahold_x_pos = 400;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetMouseButtonDown(0)) {
+      this.rotateSpeed = 10;
+      if (Input.mousePosition.x < threahold_x_pos) {
+        this.rotateSpeed = -this.rotateSpeed;
+      }
+      Debug.Log(this.rotateSpeed);
+      transform.Rotate(0, 0, this.rotateSpeed); // 
+    }
+
+	}
+}
