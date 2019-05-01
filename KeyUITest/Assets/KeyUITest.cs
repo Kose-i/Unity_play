81d1e8d98405424abb5f8f2732f3659a6839eb88
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-01 11:27:36 +0900

    Add KeyUI interface

---
+++ KeyUITest/Assets/KeyUITest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyUITest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      Debug.Log("Space-Key Down");
+    } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+      Debug.Log("RightArrow-Key Down");
+    } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+      Debug.Log("LeftArrow-Key Down");
+    }
+	}
+}
