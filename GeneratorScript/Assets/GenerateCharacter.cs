af51cfb0dd86120bd495f914d8551261f6223b82
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-02 14:33:54 +0900

    Add Prefab Script

---
+++ GeneratorScript/Assets/GenerateCharacter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateCharacter : MonoBehaviour {
+
+  public GameObject characterPrefab;
+
+  float span = 1.0f;
+  float delta = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    this.delta += Time.deltaTime;
+    if (this.delta > this.span) {
+      this.delta = 0;
+      GameObject go = Instantiate(characterPrefab) as GameObject;
+      int py = Random.Range(-3, 3);
+      go.transform.position = new Vector3(7, py, 0);
+    }
+	}
+
+}
