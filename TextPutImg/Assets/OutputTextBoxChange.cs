60b0fdecb69901325dd9f5134fbffe4b8486aa05
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-02 13:53:16 +0900

    Add TextPut Unity

---
+++ TextPutImg/Assets/OutputTextBoxChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class OutputTextBoxChange : MonoBehaviour {
+
+  GameObject textBox;
+
+	// Use this for initialization
+	void Start () {
+    this.textBox = GameObject.Find("OutputTextBox");
+    this.textBox.GetComponent<Text>().text = "Push SpaceKey";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      this.textBox.GetComponent<Text>().text = "settup";
+    }
+	}
+}
