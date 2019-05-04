d562435f7b1c6221c892156c7ea87d10c710802a
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-04 21:55:25 +0900

    Add Collision play

---
+++ CollisionCheck/Assets/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+  public static float x_diff = 0.05f;
+  public static float zero_f = 0.0f;
+
+  GameObject player;
+
+	// Use this for initialization
+	void Start () {
+    this.player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.Translate(-x_diff, zero_f, zero_f);
+    if (transform.position.x < -10.0f) {
+      Destroy(gameObject);
+    }
+
+    Vector2 p1 = transform.position; // Enemy pos
+    Vector2 p2 = this.player.transform.position; // Player pos
+    Vector2 diff = p1 - p2;
+    float d = diff.magnitude;
+    if (d < 2f) { // collision play
+      Destroy(gameObject);
+    }
+	}
+}
---
+++ CollisionCheck/Assets/MovePlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+  public float x_pos;
+  public float y_pos;
+
+  public static float thread_left = 0f;
+  public static float thread_right = 20f;
+  public static float thread_top = 4f;
+  public static float thread_buttom = -4f;
+
+  float x_move_diff = 0.5f;
+  float y_move_diff = 0.5f;
+  float zero_f = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+    this.x_pos = 0f;
+    this.y_pos = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+      if (thread_left < this.x_pos) {
+        transform.Translate(-x_move_diff,zero_f,zero_f);
+        x_pos -= x_move_diff;
+      }
+    } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+      if (this.x_pos < thread_right) {
+        transform.Translate(x_move_diff, zero_f, zero_f);
+        x_pos += x_move_diff;
+      }
+    } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+      if (thread_buttom < this.y_pos) {
+        transform.Translate(zero_f, -y_move_diff, zero_f);
+        y_pos -= y_move_diff;
+      }
+    } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+      if (this.y_pos < thread_top) {
+        transform.Translate(zero_f, y_move_diff, zero_f);
+        y_pos += y_move_diff;
+      }
+    }
+	}
+}
