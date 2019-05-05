30f0f8627a1e021a4bb04e08d32adb3f3aa3d2c1
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-05-05 21:57:13 +0900

    Add SceneManager

---
+++ SceneManager/Assets/SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+using SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (Input.GetMouseButton(0)) {
+      SceneManager.LoadScene("FinalScene");
+    }
+	}
+}
